3872d2b63e186eeb868cad9a8d4feec2b7b4ef40
Sebastian Walter <sebastian@sebastian-walter.net>
2025-08-12 01:17:10 +0200

    add success toast

--- index.php
+++ index.php
@@ -1,5 +1,10 @@
 <?php
 $eventInfo = json_decode(file_get_contents("./shifts.json"), true);
-//var_dump($eventInfo);
+if(isset($_POST["action"])) {
+    $toast = array(
+        "message" => "Deine Registrierung wurde gespeichert. Falls Du Dich austragen möchtest, kannst Du das über den Link in der Bestätigungsmail tun.",
+        "style" => "success"      
+    );
+}
 include("template.htm");
 ?>
