6e9828cace234e57240ff22ee8fac4c58fb4c36d
Sebastian Walter <sebastian@sebastian-walter.net>
2025-08-15 22:10:13 +0200

    pos-fix: disable caching for pdf export

--- src/export.php
+++ src/export.php
@@ -94,5 +94,9 @@ function handleExport($config, &$eventInfo) {
             $slot++;
         }
     }
-    $pdf->Output();
+    /* force uncached output */
+    header("Cache-Control: no-store, no-cache, must-revalidate, max-age=0");
+    header("Cache-Control: post-check=0, pre-check=0", false);
+    header("Pragma: no-cache");
+    $pdf->Output("I");
 }
